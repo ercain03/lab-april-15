1ac95ce35771388b8ad86edc9ad6d7c4eb676083
ercain03 <ercain3@gmail.com>
2016-04-15 15:03:18 -0700

    Added problem number 3 and HTML mark up

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
 function sum(num1, num2) {
   var sumTwoNumbers = num1 + num2;
-  console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sumTwoNumbers);
+  //console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sumTwoNumbers);
   var twoNumber = document.getElementById('twoNumber');
   twoNumber.textContent = '"The sum of ' + num1 + ' and ' + num2 + ' is ' + sumTwoNumbers + '."';
+  return sumTwoNumbers;
 }
 
 function multiply(num1, num2) {
   var sumTwoNumbers = num1 * num2;
-  console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sumTwoNumbers);
+  //console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sumTwoNumbers);
   var twoNumberMultiply = document.getElementById('twoMultiply');
   twoNumberMultiply.textContent = '"The sum of ' + num1 + ' and ' + num2 + ' is ' + sumTwoNumbers + '."';
+  return sumTwoNumbers;
+}
+
+function sumAndMultiply(num1, num2, num3) {
+  var myArray = [];
+  myArray.push(sum(sum(num1, num2), num3));
+  myArray.push(multiply(multiply(num1, num2), num3));
+
+  console.log(num1 + ' and ' + num2 + ' and ' + num3 + ' sum to ' + myArray[0]);
+  console.log('The numbers ' + num1 + ' and ' + num2 + ' and ' + num3 + ' have a product of: ' + myArray[1]);
+
+  var twoNumberSums = document.getElementById('twoSums');
+  twoNumberSums.textContent = '"' + num1 + ' and ' + num2 + ' and ' + num3 + ' sum to ' + myArray[0] + '"';
+
+  var twoNumberMulty = document.getElementById('twoMulty');
+  twoNumberMulty.textContent = '"The numbers ' + num1 + ' and ' + num2 + ' and ' + num3 + ' have a product of: ' + myArray[1] + '"';
+
+  return myArray;
 }
