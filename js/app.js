06b2bfd12f27c29e627568b6e1ea6cc95ec3adf9
ercain03 <ercain3@gmail.com>
2016-04-15 13:55:29 -0700

    Added a function that took in two perameters and multiplied them.

--- js/app.js
+++ js/app.js
@@ -4,3 +4,10 @@ function sum(num1, num2) {
   var twoNumber = document.getElementById('twoNumber');
   twoNumber.textContent = '"The sum of ' + num1 + ' and ' + num2 + ' is ' + sumTwoNumbers + '."';
 }
+
+function multiply(num1, num2) {
+  var sumTwoNumbers = num1 * num2;
+  console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sumTwoNumbers);
+  var twoNumberMultiply = document.getElementById('twoMultiply');
+  twoNumberMultiply.textContent = '"The sum of ' + num1 + ' and ' + num2 + ' is ' + sumTwoNumbers + '."';
+}
