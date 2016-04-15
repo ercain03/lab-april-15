0a394e6ebde510bf9bb1359f476acc4db2cee8ad
ercain03 <ercain3@gmail.com>
2016-04-15 13:49:46 -0700

    Added problem number one to app.js and put id on HTML

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function sum(num1, num2) {
+  var sumTwoNumbers = num1 + num2;
+  console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sumTwoNumbers);
+  var twoNumber = document.getElementById('twoNumber');
+  twoNumber.textContent = '"The sum of ' + num1 + ' and ' + num2 + ' is ' + sumTwoNumbers + '."';
+}
